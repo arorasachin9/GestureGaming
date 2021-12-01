c496cf4430918360178c24075d5fd29889da80d1
arorasachin9 <arorasachin977@gmail.com>
2021-12-01 12:18:13 +0530

    Commit

--- Hand_Detector.py
+++ Hand_Detector.py
@@ -2,7 +2,7 @@ import cv2
 import mediapipe as mp
 import os
 class handDetector():
-    def __init__(self,mode=False, maxHands=2, detectionCon=0.5, trackCon=0.5):
+    def __init__(self,mode=False, maxHands=2, detectionCon=75, trackCon=0.5):
         self.mode = mode
         self.maxHands = maxHands
         self.detectionCon = detectionCon
